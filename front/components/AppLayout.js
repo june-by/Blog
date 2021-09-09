041ef638c6b77667aa87be55cb42ae5132381995
ByeongJun <neostgeart@ajou.ac.kr>
2021-09-09 21:16:20 +0900

    Add IO icon

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -11,6 +11,7 @@ import { RiComputerFill , RiErrorWarningLine} from 'react-icons/ri'
 import { FiDatabase,FiServer } from 'react-icons/fi';
 import {AiOutlineHtml5,AiFillCar} from 'react-icons/ai';
 import { FaNodeJs } from 'react-icons/fa';
+import {IoSchoolOutline} from 'react-icons/io5';
 import {TiFlowSwitch} from 'react-icons/ti';
 import {GiVendingMachine} from 'react-icons/gi';
 import {DiCss3} from 'react-icons/di';
