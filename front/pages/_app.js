3f381136760ed7a13fce36ea63e12f324de84ae0
ByeongJun <neostgeart@ajou.ac.kr>
2021-09-23 22:11:28 +0900

    Add googleAdsense

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -15,6 +15,7 @@ const ByJuun = ({ Component }) => {
                 <title>BY-juun Blog</title>
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/base16/rebecca.min.css"/>
+                <script data-ad-client="ca-pub-5441996260068044" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script> 
                 <script>hljs.highlightAll();</script>
                 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
