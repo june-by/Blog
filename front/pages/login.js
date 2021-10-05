e009a8b971c486c986edcb7f8bfd34918e477f33
ByeongJun <neostgeart@ajou.ac.kr>
2021-10-05 15:55:48 +0900

    Update Login ServerSideRendering

--- front/pages/login.js
+++ front/pages/login.js
@@ -4,14 +4,30 @@ import Head from 'next/head';
 import LoginForm from '../components/LoginForm';
 import { useSelector } from 'react-redux';
 import Router from 'next/router';
+import { LOAD_MY_INFO_REQUEST } from '../reducers/user'
+import wrapper from "../store/configureStore";
+import { END } from 'redux-saga';
+import axios from 'axios'
+import { message } from 'antd';
 
 const Login = () => {
 
-    const {me} = useSelector((state)=>state.user);
+    const {me, logInDone} = useSelector((state)=>state.user);
     const id = me?.id;
     useEffect(()=>{
         if(me && id){
-        Router.replace('/');
+            if(logInDone){
+                message.success("로그인이 완료되었습니다! 메인화면으로 이동합니다.");
+                setTimeout(()=>{
+                    Router.replace('/');
+                },1000);
+            }else{
+                message.error("이미 로그인한 사용자입니다. 메인화면으로 이동합니다.");
+                setTimeout(()=>{
+                    Router.replace('/');
+                },1000);
+            }
+            
         }
     },[me,id])
 
@@ -27,5 +43,19 @@ const Login = () => {
         </>
     );
 }
+
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+    const cookie = context.req ? context.req.headers.cookie : '';
+    axios.defaults.headers.Cookie = '';
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    context.store.dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    })
+    context.store.dispatch(END);
+    await context.store.sagaTask.toPromise();
+  });
 
 export default Login;
