c03ef66acc7495f37fc07025666db8426a1e10c5
ByeongJun <neostgeart@ajou.ac.kr>
2022-05-26 22:45:05 +0900

    Update TopicNavigator ScrollBar Display

--- client/components/Block/Post/PostTop/index.tsx
+++ client/components/Block/Post/PostTop/index.tsx
@@ -10,57 +10,67 @@ import PostEditBtn from "../../../Atom/PostEditBtn";
 import styles from "./styles.module.scss";
 
 interface Props {
-	Post: PostType;
+  Post: PostType;
 }
 
 const PostTop = ({ Post }: Props) => {
-	const gotoPage = useGotoPage();
-	const router = useRouter();
-	const { title, createdAt, category, Tags, id } = Post;
-	const { data: UserInfo } = useGetUserInfo();
+  const gotoPage = useGotoPage();
+  const router = useRouter();
+  const { title, createdAt, category, Tags, id } = Post;
+  const { data: UserInfo } = useGetUserInfo();
 
-	const onClickTag = useCallback(
-		(tag) => () => {
-			router.push({
-				pathname: "/filter",
-				query: { tag: tag },
-			});
-		},
-		[router]
-	);
+  const onClickTag = useCallback(
+    (tag) => () => {
+      router.push({
+        pathname: "/filter",
+        query: { tag: tag },
+      });
+    },
+    [router]
+  );
 
-	return (
-		<div className={styles.PostTop}>
-			<div className={styles.TopArea}>
-				<div>
-					<GoBackBtn />
-				</div>
-				<div>
-					{UserInfo?.nickname === "By_juun" && (
-						<>
-							<PostDelBtn />
-							<PostEditBtn id={id} />
-						</>
-					)}
-				</div>
-			</div>
-			<h1 className={styles.PostTitle}>{title}</h1>
-			<div className={styles.AdditionalInfo}>
-				<span className={styles.AdditionalInfo_createdAt}>{dateForm(createdAt)}</span>
-				<button onClick={gotoPage(`/category/${category}`)}>{category}</button>
-				<div className={styles.AdditionalInfo_Tag}>
-					{Tags.length !== 0 &&
-						Tags.map((tag) => {
-							return (
-								<span key={tag?.id} onClick={onClickTag(tag?.content)}>
-									#{tag?.content}{" "}
-								</span>
-							);
-						})}
-				</div>
-			</div>
-		</div>
-	);
+  const onClickCategory = useCallback(
+    (category: string) => () => {
+      router.push({
+        pathname: "/filter",
+        query: { category: category },
+      });
+    },
+    []
+  );
+
+  return (
+    <div className={styles.PostTop}>
+      <div className={styles.TopArea}>
+        <div>
+          <GoBackBtn />
+        </div>
+        <div>
+          {UserInfo?.nickname === "By_juun" && (
+            <>
+              <PostDelBtn />
+              <PostEditBtn id={id} />
+            </>
+          )}
+        </div>
+      </div>
+      <h1 className={styles.PostTitle}>{title}</h1>
+      <div className={styles.AdditionalInfo}>
+        <span className={styles.AdditionalInfo_createdAt}>{dateForm(createdAt)}</span>
+        <button onClick={onClickCategory(category)}>{category}</button>
+        <div className={styles.AdditionalInfo_Tag}>
+          {Tags.length !== 0 &&
+            Tags.map((tag) => {
+              return (
+                <span key={tag?.id} onClick={onClickTag(tag?.content)}>
+                  #{tag?.content}{" "}
+                </span>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostTop;
