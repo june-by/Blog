4be3b1c5922ce117541497ff54db3bdd1c2855b4
ByeongJun <neostgeart@ajou.ac.kr>
2022-05-27 01:13:00 +0900

    Add og:image, image quality higher,

--- client/Hooks/useGetTopicArr.ts
+++ client/Hooks/useGetTopicArr.ts
@@ -1,29 +1,39 @@
 import { useEffect } from "react";
 
 const useGetTopicArr = (setTopicArr: React.Dispatch<React.SetStateAction<never[]>>) => {
-	useEffect(() => {
-		const $h3Tags = document.getElementsByTagName('h3');
-		const $h2Tags = document.getElementsByTagName('h2');
-		const tagArr = [];
-		if ($h3Tags.length !== 0) {
-			for (const tag of $h3Tags as any) {
-				if (tag.innerText.length !== 1) {
-					tagArr.push(tag);
-				}
-			}
-		}
+  useEffect(() => {
+    const $h1Tags = document.getElementsByTagName("h1");
+    const $h2Tags = document.getElementsByTagName("h2");
+    const $h3Tags = document.getElementsByTagName("h3");
+    const tagArr = [];
 
-		if ($h2Tags.length !== 0) {
-			for (const tag of $h2Tags as any) {
-				if (tag.innerText.length !== 1) {
-					tagArr.push(tag);
-				}
-			}
-		}
+    if ($h1Tags.length !== 0) {
+      for (const tag of $h1Tags as any) {
+        if (tag.innerText.length !== 1) {
+          tagArr.push(tag);
+        }
+      }
+    }
 
-		tagArr.sort((a, b) => a.getBoundingClientRect().top - b.getBoundingClientRect().top)
-		setTopicArr(tagArr as never);
-	}, [])
+    if ($h2Tags.length !== 0) {
+      for (const tag of $h2Tags as any) {
+        if (tag.innerText.length !== 1) {
+          tagArr.push(tag);
+        }
+      }
+    }
+
+    if ($h3Tags.length !== 0) {
+      for (const tag of $h3Tags as any) {
+        if (tag.innerText.length !== 1) {
+          tagArr.push(tag);
+        }
+      }
+    }
+
+    tagArr.sort((a, b) => a.getBoundingClientRect().top - b.getBoundingClientRect().top);
+    setTopicArr(tagArr as never);
+  }, []);
 };
 
 export default useGetTopicArr;
