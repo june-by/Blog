25fb722b572c277b76e7abf775816afd33d05601
ByeongJun <neostgeart@ajou.ac.kr>
2022-05-29 15:25:38 +0900

    Delete Console log, Add useSetProgressState Hook

---
+++ client/Hooks/useSetProgressState.ts
@@ -0,0 +1,23 @@
+import { Router } from "next/router";
+import React, { useEffect } from "react";
+
+const useSetProgressState = (setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+    const end = () => {
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+};
+
+export default useSetProgressState;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,37 +3,17 @@ import "../styles/Editor.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Head from "next/head";
 import Header from "../components/Layout/Header";
-import { Router, useRouter } from "next/router";
 import Loading from "../utils/Loading";
 import ProgressBar from "../components/Atom/ProgressBar";
+import useSetProgressState from "../Hooks/useSetProgressState";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   const [loading, setLoading] = useState<boolean>(false);
-  const router = useRouter();
-  useEffect(() => {
-    const start = () => {
-      console.log(router);
-      console.log("start");
-      setLoading(true);
-    };
-    const end = () => {
-      console.log(router);
-      console.log("findished");
-      setLoading(false);
-    };
-    Router.events.on("routeChangeStart", start);
-    Router.events.on("routeChangeComplete", end);
-    Router.events.on("routeChangeError", end);
-    return () => {
-      Router.events.off("routeChangeStart", start);
-      Router.events.off("routeChangeComplete", end);
-      Router.events.off("routeChangeError", end);
-    };
-  }, [router]);
+  useSetProgressState(setLoading);
   return (
     <>
       {loading ? (
