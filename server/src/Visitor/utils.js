1cfc7da4d72d63412ca476af96726739e49e0c58
ByeongJun <neostgeart@ajou.ac.kr>
2022-10-29 18:28:26 +0900

    Delete: js file, Update: tsconfig.json
    빌드로 생성된 자바스크립트 파일 제거
    tsconfig에서 outDir 설정

--- server/src/Visitor/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getVisitorDateInfo = void 0;
-var getVisitorDateInfo = function () {
-    var nowDate = new Date();
-    return String(nowDate.getFullYear()) + String(nowDate.getMonth()) + String(nowDate.getDate());
-};
-exports.getVisitorDateInfo = getVisitorDateInfo;
