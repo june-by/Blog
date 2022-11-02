db8783030c8487d9ba2240010b531aa003a5f0e1
ByeongJun <neostgeart@ajou.ac.kr>
2022-11-03 03:02:31 +0900

    Fix: isAdmin MiddleWare
    해당 미들웨어에 오류가 발생해 에러 추적을 위해 수정

--- server/build/middleWare/isAdmin.js
+++ server/build/middleWare/isAdmin.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.isAdmin = void 0;
 var isAdmin = function (req, res, next) {
     var id = req.user.id;
-    if (String(id) === process.env.ADMIN_ID) {
-        next();
-    }
-    else {
-        res.status(401).send("관리자만 이용 가능합니다.");
-    }
+    console.log(req.user);
+    next();
+    // if (String(id) === process.env.ADMIN_ID) {
+    //   next();
+    // } else {
+    //   res.status(401).send("관리자만 이용 가능합니다.");
+    // }
 };
 exports.isAdmin = isAdmin;
--- server/middleWare/isAdmin.ts
+++ server/middleWare/isAdmin.ts
@@ -2,9 +2,11 @@ import { NextFunction, Request, Response } from "express";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.user as { id: number };
-  if (String(id) === process.env.ADMIN_ID) {
-    next();
-  } else {
-    res.status(401).send("관리자만 이용 가능합니다.");
-  }
+  console.log(req.user);
+  next();
+  // if (String(id) === process.env.ADMIN_ID) {
+  //   next();
+  // } else {
+  //   res.status(401).send("관리자만 이용 가능합니다.");
+  // }
 };
