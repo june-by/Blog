63b2ab123e37b7d51649e29a6a8d5f06f76b6753
By_juun <neostgeart@gmail.com>
2023-01-21 20:30:45 +0900

    [Test] 모바일에서 사용되는 CategoryRow 컴포넌트 테스트 코드 추가

--- client/components/Layout/MobileMenu/MobileCategoryList/CategoryRow/index.tsx
+++ client/components/Layout/MobileMenu/MobileCategoryList/CategoryRow/index.tsx
@@ -1,6 +1,5 @@
 import useGotoPage from "Hooks/useGotoPage";
-import { useRouter } from "next/router";
-import React, { useCallback } from "react";
+import React from "react";
 import styles from "./styles.module.scss";
 
 interface Props {
@@ -12,7 +11,12 @@ const CategoryRow = ({ category, length }: Props) => {
   const gotoPage = useGotoPage();
 
   return (
-    <div key={category} onClick={gotoPage(`/category/${category}`)} className={styles.CategoryItem}>
+    <div
+      data-testid="CategoryRow"
+      key={category}
+      onClick={gotoPage(`/category/${category}`)}
+      className={styles.CategoryItem}
+    >
       <div className={styles.text}>{category}</div>
       <div className={styles.count}>{length}</div>
     </div>
