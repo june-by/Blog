9b57b87bf252ddcfe04499897dcd2d43ddf2069c
By_juun <neostgeart@gmail.com>
2023-01-24 16:46:44 +0900

    [Feat] 최근 댓글 관련 API 에러 처리 추가

--- client/services/Comment/index.ts
+++ client/services/Comment/index.ts
@@ -1,6 +1,10 @@
 import { customAxios } from "utils/CustomAxios";
 
 export const getRecentCommentAPI = async () => {
-  const { data } = await customAxios.get("/comment/recent");
-  return data;
+  try {
+    const { data } = await customAxios.get("/comment/recent");
+    return data;
+  } catch (err) {
+    throw new Error();
+  }
 };
