5853c43b780890c7dcab8732cc40d0b1754f8256
By_juun <neostgeart@gmail.com>
2023-02-05 22:30:40 +0900

    [Fix] githubCallback Page 제거

--- client/components/Block/SocialLoginBtns/index.tsx
+++ client/components/Block/SocialLoginBtns/index.tsx
@@ -2,15 +2,14 @@ import GithubIcon from "components/Icon/github";
 import KaKaoIcon from "components/Icon/kakao";
 import { ServerURL } from "constants/serverURL";
 import React from "react";
-import { customAxios } from "utils/CustomAxios";
 import styles from "./styles.module.scss";
 
-const client_id = "d4cd6b663e8f7cf8eab0";
-const redirect_url = "http://localhost:3000/githubCallback";
 const SocialLoginBtns = () => {
   const githubLogin = async () => {
     window.location.href = `${ServerURL}user/githublogin`;
-    //window.location.href = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_url}`;
+  };
+  const kakaoLogin = () => {
+    alert("카카오 로그인은 현재 준비중입니다.");
   };
   return (
     <div className={styles.SocialLoginBtns}>
@@ -19,7 +18,7 @@ const SocialLoginBtns = () => {
         <button className={styles.githubLoginBtn} onClick={githubLogin}>
           <GithubIcon />
         </button>
-        <button className={styles.kakaoLoginBtn}>
+        <button className={styles.kakaoLoginBtn} onClick={kakaoLogin}>
           <KaKaoIcon />
         </button>
       </div>
