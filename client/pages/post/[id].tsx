4749abc80bfc21bb0f00da0a990b6979c1fc1e6f
By_juun <neostgeart@gmail.com>
2023-02-26 18:26:14 +0900

    [Feat] onDemand Revalidation을 통한 SSG Revalidate 기능 추가

--- client/pages/post/[id].tsx
+++ client/pages/post/[id].tsx
@@ -100,7 +100,6 @@ export const getStaticProps: GetStaticProps = async (context: GetStaticPropsCont
     props: {
       dehydratedState: dehydrate(queryClient),
     },
-    revalidate: 60,
   };
 };
 
