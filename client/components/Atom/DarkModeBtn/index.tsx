480a8780dfefcd5919e8c01076086527c520b0a8
By_juun <neostgeart@gmail.com>
2023-04-16 17:43:50 +0900

    feat: 다크모드 버튼 아이콘 변경

--- client/components/Atom/DarkModeBtn/index.tsx
+++ client/components/Atom/DarkModeBtn/index.tsx
@@ -4,11 +4,11 @@ import useToggle from "Hooks/useToggle";
 import styles from "./styles.module.scss";
 import extractFromCookie from "utils/extractFromCookie";
 import SSRSafeSuspense from "components/_hoc/SSRSafeSuspense";
+import Light from "components/Icon/light";
+import Dark from "components/Icon/dark";
 
 const DARK_MODE = false;
 const LIGHT_MODE = true;
-const DARK_MODE_POS = "27px";
-const LIGHT_MODE_POS = "1px";
 
 const DarkModeBtn = () => {
   const toggleThumbRef = useRef<HTMLButtonElement>(null);
@@ -16,23 +16,16 @@ const DarkModeBtn = () => {
   useDarkMode(mode, toggleThumbRef);
 
   return (
-    <div data-testid="DarkModeBtnWrapper" className={styles.DarkModeBtnWrapper} onClick={onChange}>
-      <div className={styles.DarkModeBtn}>
-        <div className={styles.toggleTrackCheckY}>
-          <span>🌜</span>
-        </div>
-        <div className={styles.toggleTrackCheckX}>
-          <span>🌞</span>
-        </div>
-        <button
-          data-testid="toggleBtn"
-          aria-label="toggleDarkModeButton"
-          ref={toggleThumbRef}
-          style={{ left: mode === DARK_MODE ? DARK_MODE_POS : LIGHT_MODE_POS }}
-          className={styles.toggleTrackThumb}
-        ></button>
-      </div>
-    </div>
+    <button
+      data-testid="toggleBtn"
+      aria-label="toggleDarkModeButton"
+      ref={toggleThumbRef}
+      className={styles.darkmodeBtn}
+      onClick={onChange}
+    >
+      {mode === DARK_MODE && <Light />}
+      {mode === LIGHT_MODE && <Dark />}
+    </button>
   );
 };
 
