942e0ef59b6524d13d5b99d09746ad0425c39a18
By_juun <neostgeart@gmail.com>
2023-04-22 02:08:24 +0900

    refactor: mobileMenuWrap container 위치 변경

--- client/components/Header/mobileMenu/index.tsx
+++ client/components/Header/mobileMenu/index.tsx
@@ -1,12 +1,1 @@
-import MobileMenuContainer from "context/mobileMenuContext";
-import MobileMenuWrap from "./mobileMenuWrap";
-
-const MobileMenu = () => {
-  return (
-    <MobileMenuContainer>
-      <MobileMenuWrap />
-    </MobileMenuContainer>
-  );
-};
-
-export default MobileMenu;
+export { default } from "./mobileMenuWrap";
