9b0cc59aa5f119b65b2a6e232a59227e5762e4b3
By_juun <neostgeart@gmail.com>
2023-04-22 22:03:55 +0900

    refactor: useDarkMode

--- client/components/Header/DarkModeButton/useDarkMode.ts
+++ client/components/Header/DarkModeButton/useDarkMode.ts
@@ -9,22 +9,9 @@ function useDarkMode(mode: boolean) {
 }
 
 function changeMode(mode: boolean) {
-  const { theme } = MODE_VALUE[changeModeBoolToStr(mode)];
+  const theme = mode === LIGHT ? "light" : "dark";
   document.body.dataset.theme = theme;
   document.cookie = `theme=${theme}; path=/`;
-}
-
-interface MODE_VALUE_TYPE {
-  [key: string]: { theme: string };
-}
-
-const MODE_VALUE: MODE_VALUE_TYPE = {
-  LIGHT: { theme: "light" },
-  DARK: { theme: "dark" },
-};
-
-function changeModeBoolToStr(mode: boolean) {
-  return mode === LIGHT ? "DARK" : "LIGHT";
 }
 
 export default useDarkMode;
