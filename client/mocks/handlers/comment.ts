9f6359a9f9096dc0811489cd44e173823538595d
By_juun <neostgeart@gmail.com>
2023-04-30 18:33:33 +0900

    feat: 최근 댓글 모킹 데이터 추가

--- client/mocks/handlers/comment.ts
+++ client/mocks/handlers/comment.ts
@@ -1,23 +1,11 @@
 import { rest } from "msw";
 import { ServerURL } from "constants/serverURL";
+import { RECENT_COMMENT_MOCK_DATA } from "mocks/data/comment";
 
 export const postComment = rest.post(`${ServerURL}/post/:postId/comment`, (req, res, ctx) => {
   return res(ctx.status(200), ctx.text("성공"));
 });
 
 export const getRecentComment = rest.get(`${ServerURL}/comment/recent`, (req, res, ctx) => {
-  return res(
-    ctx.status(200),
-    ctx.json(
-      Array.from({ length: 10 }, (_, idx) => {
-        return {
-          id: idx,
-          content: `testComment${idx}`,
-          Post: {
-            id: idx,
-          },
-        };
-      })
-    )
-  );
+  return res(ctx.status(200), ctx.json(RECENT_COMMENT_MOCK_DATA));
 });
