a1d2e7b5e061f49359ffc2eb2fa5ed34e0343445
By_juun <neostgeart@gmail.com>
2023-04-30 18:26:29 +0900

    test: '조회수 Top10을 보여주는 사이드바가 있어야 한다.' 테스트 내부 기준 수정

--- client/tests/home.e2e.test.ts
+++ client/tests/home.e2e.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { TOP_VIEWS_POST_MOCK_DATA } from "mocks/data/post";
 import { VISITOR_MOCK_DATA } from "mocks/data/visitor";
 import HomePOM from "./home";
 
@@ -36,6 +37,10 @@ test("조회수 Top10을 보여주는 사이드바가 있어야 한다.", async 
   const TopViewPostsBlock = page.getByText("조회수 Top10");
 
   await expect(TopViewPostsBlock).toBeVisible();
+
+  for (let idx = 0; idx < 10; idx++) {
+    await expect(Home.page.getByText(`${idx + 1}. ${TOP_VIEWS_POST_MOCK_DATA[idx].title}`)).toBeVisible();
+  }
 });
 
 test("최근 댓글을 보여주는 사이드바가 있어야 한다", async ({ page }) => {
