c3ae6f500bf2fac5643af9297d96b9e3c97d1b72
By_juun <neostgeart@gmail.com>
2023-05-06 22:45:53 +0900

    feat: 로그인 API 모킹 추가

--- client/tests/posts.ts
+++ client/tests/posts.ts
@@ -4,6 +4,7 @@ import { ServerURL } from "constants/serverURL";
 import { RECENT_COMMENT_MOCK_DATA } from "mocks/data/comment";
 import { CATEGORY_LENGTH_MOCK_DATA, MAIN_POSTS_MOCK_DATA, TOP_VIEWS_POST_MOCK_DATA } from "mocks/data/post";
 import { RECENT_TAG_MOCK_DATA } from "mocks/data/tag";
+import { USER_MOCK_DATA } from "mocks/data/user";
 import { VISITOR_MOCK_DATA } from "mocks/data/visitor";
 
 export default class PostsPOM {
@@ -61,6 +62,14 @@ export default class PostsPOM {
     await this.page.route(`${ServerURL}/posts/load/main/1`, async (route) => {
       await route.fulfill({
         json: MAIN_POSTS_MOCK_DATA,
+      });
+    });
+
+    await this.page.route(`${ServerURL}/user/login`, async (route) => {
+      if (route.request().method() !== "POST") return;
+
+      await route.fulfill({
+        json: USER_MOCK_DATA,
       });
     });
   }
