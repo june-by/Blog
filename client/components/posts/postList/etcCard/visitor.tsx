70a942c26a0485819f19214164afbc35b2e3f669
By_juun <neostgeart@gmail.com>
2023-05-07 20:50:45 +0900

    style: 방문 카드와 최근 댓글 카드 분리

--- client/components/posts/postList/etcCard/visitor.tsx
+++ client/components/posts/postList/etcCard/visitor.tsx
@@ -1,5 +1,6 @@
 import { useGetVisitor } from "Hooks/Visitor";
 import React from "react";
+import styles from "./styles.module.scss";
 
 const Visitor = () => {
   const { data: visitorData } = useGetVisitor();
@@ -7,15 +8,15 @@ const Visitor = () => {
   if (!visitorData) return null;
 
   return (
-    <>
-      <h3>방문객</h3>
+    <div className={styles.visitorCard}>
+      <h3>방문</h3>
       <span>
         총 방문 : <strong>{visitorData?.totalVisitor}</strong>명
       </span>
       <span>
         오늘 방문 : <strong>{visitorData?.todayVisitor}</strong>명
       </span>
-    </>
+    </div>
   );
 };
 
