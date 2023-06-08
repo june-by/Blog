e7ed83966914a38b6e75100be367ff4553093714
By_juun <78716842+BY-juun@users.noreply.github.com>
2023-06-08 14:25:08 +0900

    feat: 게시글 관련 에러 처리 로직(alert, history replace)을 훅 내부로 이관

--- client/Hooks/Post/index.ts
+++ client/Hooks/Post/index.ts
@@ -12,7 +12,13 @@ import {
 } from "services/post";
 import { useInfiniteQuery, useMutation, useQuery } from "react-query";
 import { getMainPostsAPI } from "services/post";
-import { AddPostParams, CategoryCount, PostsType, PostType, TopViewsPost } from "Types/post";
+import {
+  AddPostParams,
+  CategoryCount,
+  PostsType,
+  PostType,
+  TopViewsPost,
+} from "Types/post";
 import { useRouter } from "next/router";
 import QUERY_KEY from "constants/queryKey";
 import CACHE_OPTION from "constants/cacheOption";
@@ -22,19 +28,43 @@ import { CommentType } from "Types/comment";
 import { getCommentAPI } from "services/comment";
 
 export const useGetMainPost = () =>
-  useInfiniteQuery<Array<PostsType>>([QUERY_KEY.POST.MAIN], ({ pageParam = 1 }) => getMainPostsAPI(pageParam), {
-    ...CACHE_OPTION.ALL,
-    getNextPageParam: (lastPage, allPage) => (lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1),
-  });
+  useInfiniteQuery<Array<PostsType>>(
+    [QUERY_KEY.POST.MAIN],
+    ({ pageParam = 1 }) => getMainPostsAPI(pageParam),
+    {
+      ...CACHE_OPTION.ALL,
+      getNextPageParam: (lastPage, allPage) =>
+        lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1,
+    }
+  );
 
-export const useGetOnePost = (id: number) =>
-  useQuery<PostType | null>([QUERY_KEY.POST.ONE, id], () => getOnePostAPI(id), CACHE_OPTION.ALL);
-
+export const useGetOnePost = (id: number) => {
+  const router = useRouter();
+  return useQuery<PostType | null>(
+    [QUERY_KEY.POST.ONE, id],
+    () => getOnePostAPI(id),
+    {
+      ...CACHE_OPTION.ALL,
+      onError(err: any) {
+        alert(err);
+        router.replace("/");
+      },
+    }
+  );
+};
 export const useGetTopViewsPosts = () =>
-  useQuery<TopViewsPost[]>([QUERY_KEY.POST.TOPVIEWS], () => GetTopViewsPostsAPI(), CACHE_OPTION.WITHOUT_FETCH_ON_MOUNT);
+  useQuery<TopViewsPost[]>(
+    [QUERY_KEY.POST.TOPVIEWS],
+    () => GetTopViewsPostsAPI(),
+    CACHE_OPTION.WITHOUT_FETCH_ON_MOUNT
+  );
 
 export const useGetAllCateogryLength = () =>
-  useQuery<Array<CategoryCount>>([QUERY_KEY.POST.CATEGORY_LENGTH], () => getAllCategoryLengthAPI(), CACHE_OPTION.ALL);
+  useQuery<Array<CategoryCount>>(
+    [QUERY_KEY.POST.CATEGORY_LENGTH],
+    () => getAllCategoryLengthAPI(),
+    CACHE_OPTION.ALL
+  );
 
 export const useGetCategoryPosts = (category: string | string[] | undefined) =>
   useInfiniteQuery<Array<PostsType>>(
@@ -42,7 +72,8 @@ export const useGetCategoryPosts = (category: string | string[] | undefined) =>
     ({ pageParam = 1 }) => getCategoryPostAPI(category, pageParam),
     {
       ...CACHE_OPTION.ALL,
-      getNextPageParam: (lastPage, allPage) => (lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1),
+      getNextPageParam: (lastPage, allPage) =>
+        lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1,
     }
   );
 
@@ -52,21 +83,35 @@ export const useGetSearchPosts = (search: string | string[] | undefined) =>
     ({ pageParam = 1 }) => getSearchPostAPI(search, pageParam),
     {
       ...CACHE_OPTION.ALL,
-      getNextPageParam: (lastPage, allPage) => (lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1),
+      getNextPageParam: (lastPage, allPage) =>
+        lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1,
     }
   );
 
 export const useGetTagPosts = (tag: string | string[] | undefined) =>
-  useInfiniteQuery<Array<PostsType>>([QUERY_KEY.POST.TAG, tag], ({ pageParam = 1 }) => getTagPostAPI(tag, pageParam), {
-    ...CACHE_OPTION.ALL,
-    getNextPageParam: (lastPage, allPage) => (lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1),
-  });
+  useInfiniteQuery<Array<PostsType>>(
+    [QUERY_KEY.POST.TAG, tag],
+    ({ pageParam = 1 }) => getTagPostAPI(tag, pageParam),
+    {
+      ...CACHE_OPTION.ALL,
+      getNextPageParam: (lastPage, allPage) =>
+        lastPage.length < POSTS_PER_PAGE ? undefined : allPage.length + 1,
+    }
+  );
 
 export const useGetPostComments = (id: number) =>
-  useQuery<{ comments: CommentType[] }>([QUERY_KEY.COMMNET.ONE, id], () => getCommentAPI(id), CACHE_OPTION.ALL);
+  useQuery<{ comments: CommentType[] }>(
+    [QUERY_KEY.COMMNET.ONE, id],
+    () => getCommentAPI(id),
+    CACHE_OPTION.ALL
+  );
 
 export const useGetPostViewCount = (id: number) =>
-  useQuery<{ viewCount: number }>([QUERY_KEY.POST.VIEWCOUNT, id], () => getPostViewCountAPI(id), CACHE_OPTION.ALL);
+  useQuery<{ viewCount: number }>(
+    [QUERY_KEY.POST.VIEWCOUNT, id],
+    () => getPostViewCountAPI(id),
+    CACHE_OPTION.ALL
+  );
 
 export const useAddPost = () => {
   return useMutation(AddPostAPI, {
@@ -83,16 +128,19 @@ export const useAddPost = () => {
 
 export const useEditPost = () => {
   const { query } = useRouter();
-  return useMutation((reqData: AddPostParams) => EditPostAPI(reqData, Number(query.id)), {
-    onMutate: () => {
-      document.body.style.cursor = "wait";
-    },
-    onSuccess: () => {
-      document.body.style.cursor = "default";
-      alert(MESSAGE.POST_EDIT_SUCCESS);
-      return window.location.replace(`/post/${query.id}`);
-    },
-  });
+  return useMutation(
+    (reqData: AddPostParams) => EditPostAPI(reqData, Number(query.id)),
+    {
+      onMutate: () => {
+        document.body.style.cursor = "wait";
+      },
+      onSuccess: () => {
+        document.body.style.cursor = "default";
+        alert(MESSAGE.POST_EDIT_SUCCESS);
+        return window.location.replace(`/post/${query.id}`);
+      },
+    }
+  );
 };
 
 export const useDeletePost = () => {
