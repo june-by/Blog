52c594ea0abcddf580ebb3fd39c28b3d37541898
By_juun <neostgeart@gmail.com>
2023-07-01 21:46:11 +0900

    feat: renderWithContext util 함수 매게변수 기본 할당 값 추가

--- client/utils/test/renderWithContext.tsx
+++ client/utils/test/renderWithContext.tsx
@@ -2,8 +2,13 @@ import { render } from "@testing-library/react";
 import { NextRouter } from "next/router";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterContext } from "next/dist/shared/lib/router-context";
+import { createMockRouter } from "./createMockRouter";
 
-export function renderWithContext(router: NextRouter, queryClient: QueryClient, component: JSX.Element) {
+export function renderWithContext(
+  component: JSX.Element,
+  router: NextRouter = createMockRouter(),
+  queryClient: QueryClient = new QueryClient()
+) {
   render(
     <RouterContext.Provider value={router}>
       <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
