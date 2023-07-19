3d3d6b3d7a77ad470aefc2565eb5a336c96f416f
BY-juun <neostgeart@gmail.com>
2023-07-19 16:31:03 +0900

    feat: delete FloatingHeader import

--- client/components/Header/headerWrap.tsx
+++ client/components/Header/headerWrap.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import LoginModal from "components/_Modal/LoginModal";
 import SignUpModal from "components/_Modal/SignUpModal";
 import SearchModal from "components/_Modal/SearchModal";
-import FloatingHeader from "./FloatingHeader";
 import Header from "./header";
 
 const HeaderWrap = () => {
   return (
     <>
       <Header />
-      {/* <FloatingHeader /> */}
       <LoginModal />
       <SignUpModal />
       <SearchModal />
