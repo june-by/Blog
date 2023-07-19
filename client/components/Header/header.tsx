a6fce7de7ebba7f531804289098408c7a06984eb
BY-juun <neostgeart@gmail.com>
2023-07-19 13:35:17 +0900

    feat: Header Scroll Animation

--- client/components/Header/header.tsx
+++ client/components/Header/header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import styles from "./styles.module.scss";
 import IconButton from "components/shared/IconButton";
 import SearchButton from "./searchButton";
@@ -10,10 +10,15 @@ import useToggle from "Hooks/useToggle";
 import useGotoPage from "Hooks/useGotoPage";
 import { useGetUserInfo } from "Hooks/User";
 import ThemeToggleButton from "./ThemeToggleButton";
+import useScroll from "Hooks/useScroll";
 
 const ADMIN_EMAIL = "neostgeart@gmail.com";
 
+const HEADER_HEIGHT = "50px";
+
 const Header = () => {
+  const headerRef = useRef<HTMLHeadingElement | null>(null);
+
   const { data: userData } = useGetUserInfo();
   const [showEmail, _, onClickEmail] = useToggle(false);
 
@@ -21,8 +26,24 @@ const Header = () => {
 
   const gotoPage = useGotoPage();
 
+  const hideHeader = useCallback(() => {
+    if (!headerRef?.current) {
+      return;
+    }
+    headerRef.current.style.marginTop = `-${HEADER_HEIGHT}`;
+  }, []);
+
+  const showHeader = useCallback(() => {
+    if (!headerRef?.current) {
+      return;
+    }
+    headerRef.current.style.marginTop = "0px";
+  }, []);
+
+  useScroll({ onScrollDown: hideHeader, onScrollUp: showHeader });
+
   return (
-    <header className={styles.headerStyleWrap}>
+    <header ref={headerRef} className={styles.headerStyleWrap}>
       <div className={styles.headerContentWrap}>
         <div className={styles.headerLeftWrap}>
           <h1 onClick={gotoPage("/")}>ByJuun.</h1>
@@ -31,8 +52,14 @@ const Header = () => {
             aria-label="gotoGithubButton"
             onClick={() => window.open("https://github.com/BY-juun")}
           />
-          <IconButton Icon={<GoogleIcon />} aria-label="toggleEmailButton" onClick={onClickEmail} />
-          {showEmail && <div className={styles.headerLeftWrap_email}>{ADMIN_EMAIL}</div>}
+          <IconButton
+            Icon={<GoogleIcon />}
+            aria-label="toggleEmailButton"
+            onClick={onClickEmail}
+          />
+          {showEmail && (
+            <div className={styles.headerLeftWrap_email}>{ADMIN_EMAIL}</div>
+          )}
         </div>
         <div className={styles.headerRightWrap}>
           {isLoggedIn && (
