b4c03af6f3eea402525a7a221f74289176afa8de
BY-juun <neostgeart@gmail.com>
2023-07-23 20:20:37 +0900

    feat: Apply WithAdminValidation

--- client/components/post/PostHeader/PostAdminButtons/postAdminButtons.tsx
+++ client/components/post/PostHeader/PostAdminButtons/postAdminButtons.tsx
@@ -1,22 +1,20 @@
 import PostEditButton from "./postEditButton";
-import { useGetUserInfo } from "Hooks/User";
 import React from "react";
-import IsAdmin from "utils/isAdmin";
 import PostDeleteButton from "./postDeleteButton";
 import useQueryId from "Hooks/useQueryId";
 import styles from "./styles.module.scss";
+import WithAdminValidation from "components/_hoc/WithAdminValidation";
 
 const PostAdminButtons = () => {
   const postId = useQueryId();
-  const { data: UserInfo } = useGetUserInfo();
-
-  if (!IsAdmin(UserInfo)) return null;
 
   return (
-    <div className={styles.PostAdminButtons}>
-      <PostDeleteButton />
-      <PostEditButton id={postId} />
-    </div>
+    <WithAdminValidation>
+      <div className={styles.PostAdminButtons}>
+        <PostDeleteButton />
+        <PostEditButton id={postId} />
+      </div>
+    </WithAdminValidation>
   );
 };
 
