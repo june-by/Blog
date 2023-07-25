cd73adc48e0a8eb3e8b12568f5e373dd088ed666
By_juun <neostgeart@gmail.com>
2023-07-25 20:24:25 +0900

    refactor: WriteContainer Position

--- client/components/write/writeWrap.tsx
+++ client/components/write/writeWrap.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { WriteContainer } from "context/writeContext";
 import styles from "./@styles.module.scss";
 import dynamic from "next/dynamic";
 import ThumbNailPicker from "components/write/thumbNailPicker";
@@ -14,22 +15,24 @@ const Editor = dynamic(() => import("./editor"), {
 
 const WriteWrap = () => {
   return (
-    <div className={styles.Write}>
-      <div className={styles.titleArea}>
-        <TitleInput />
-        <IsPublicCheckBox />
-        <SubmitButton />
-      </div>
-      <div className={styles.etcArea}>
-        <CategorySelector />
-        <div className={styles.tagWrap}>
-          <TagForm />
-          <TagList />
+    <WriteContainer>
+      <div className={styles.Write}>
+        <div className={styles.titleArea}>
+          <TitleInput />
+          <IsPublicCheckBox />
+          <SubmitButton />
         </div>
+        <div className={styles.etcArea}>
+          <CategorySelector />
+          <div className={styles.tagWrap}>
+            <TagForm />
+            <TagList />
+          </div>
+        </div>
+        <Editor />
+        <ThumbNailPicker />
       </div>
-      <Editor />
-      <ThumbNailPicker />
-    </div>
+    </WriteContainer>
   );
 };
 
