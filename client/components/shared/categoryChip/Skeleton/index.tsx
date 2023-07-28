507084c8e8849d6f38a48011e88db6a83f2514b0
BY-juun <neostgeart@gmail.com>
2023-07-28 16:28:45 +0900

    feat: delete categoryfchip

--- client/components/shared/categoryChip/Skeleton/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../styles.module.scss";
-
-const CategoryChipSkeleton = () => {
-  return <div className={styles.CategorySkeleton}></div>;
-};
-
-export default CategoryChipSkeleton;
