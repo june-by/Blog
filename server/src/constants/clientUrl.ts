0fa8bb64962f89ef82802e57b46623a95873c6dd
By_juun <neostgeart@gmail.com>
2023-07-29 04:22:15 +0900

    feat: fix client url in dev environment

--- server/src/constants/clientUrl.ts
+++ server/src/constants/clientUrl.ts
@@ -1,3 +1,3 @@
-const CLIENT_URL = process.env.NODE_ENV === "production" ? "https://byjuun.com" : "http://localhost:3000";
+const CLIENT_URL = process.env.NODE_ENV === "production" ? "https://byjuun.com" : "https://local.byjuun.com:3000";
 
 export default CLIENT_URL;
