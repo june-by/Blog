cc849863a4115e41ebbe49da9640878ffc93647f
BY-juun <neostgeart@gmail.com>
2023-07-28 16:09:48 +0900

    feat: CategoryButton Focus and Link

--- client/components/shared/CategoryButton/CategoryButton.tsx
+++ client/components/shared/CategoryButton/CategoryButton.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./styles.module.scss";
 import { CategoryType } from "constants/category";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface Props {
   category: CategoryType;
@@ -8,11 +10,34 @@ interface Props {
 }
 
 const CategoryButton = ({ category, length }: Props) => {
+  const categoryButtonRef = useRef<HTMLAnchorElement | null>(null);
+  const { query } = useRouter();
+
+  const currentCategory = query.category;
+
+  const isCurrentSelectedCategory = currentCategory === category;
+
+  useEffect(() => {
+    if (!isCurrentSelectedCategory) return;
+    categoryButtonRef.current?.scrollIntoView({
+      block: "nearest",
+      inline: "center",
+    });
+  }, [isCurrentSelectedCategory]);
+
   return (
-    <div className={styles.CategoryButton}>
+    <Link
+      ref={categoryButtonRef}
+      className={
+        isCurrentSelectedCategory
+          ? `${styles.CategoryButton} ${styles.Selected}`
+          : `${styles.CategoryButton}`
+      }
+      href={`/posts?category=${category}`}
+    >
       <span className={styles.content}>{category}</span>
       <span className={styles.length}>{length}</span>
-    </div>
+    </Link>
   );
 };
 
