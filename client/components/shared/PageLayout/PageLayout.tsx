da41136ec20195279b418314d2206e9cc1ab44f4
BY-juun <neostgeart@gmail.com>
2023-08-16 18:19:20 +0900

    refactor: Page Layout

--- client/components/shared/PageLayout/PageLayout.tsx
+++ client/components/shared/PageLayout/PageLayout.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./styles.module.scss";
 
+interface Props {
+  children: JSX.Element;
+}
+
 const PageLayout = ({ children }: { children: JSX.Element }) => {
   return <section className={styles.PageLayout}>{children}</section>;
 };
