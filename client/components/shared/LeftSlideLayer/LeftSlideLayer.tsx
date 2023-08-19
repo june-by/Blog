9634083d6c3fbc76f9b57c156de5e3754785524f
BY-juun <neostgeart@gmail.com>
2023-08-20 07:46:19 +0900

    refactor: 상수 추출

--- client/components/shared/LeftSlideLayer/LeftSlideLayer.tsx
+++ client/components/shared/LeftSlideLayer/LeftSlideLayer.tsx
@@ -6,13 +6,16 @@ interface Props {
   className?: string;
 }
 
+const LEFT_STYLE_FOR_OPEN = "100%";
+const LEFT_STYLE_FOR_CLOSE = "0";
+
 const LeftSlideLayer = ({ children, isOpen, className }: Props) => {
   const leftSlideLayerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (!leftSlideLayerRef.current) return;
-    if (!isOpen) leftSlideLayerRef.current.style.left = "100%";
-    else leftSlideLayerRef.current.style.left = "0";
+    if (!isOpen) leftSlideLayerRef.current.style.left = LEFT_STYLE_FOR_OPEN;
+    else leftSlideLayerRef.current.style.left = LEFT_STYLE_FOR_CLOSE;
   }, [isOpen]);
 
   return (
