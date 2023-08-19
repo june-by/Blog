52cb7eb7ddee9c8314f7eacaaf810debd2a4bc2f
BY-juun <neostgeart@gmail.com>
2023-08-19 13:28:50 +0900

    feat: SearchBox

--- client/components/shared/PostsListTitle/PostsListTitle.tsx
+++ client/components/shared/PostsListTitle/PostsListTitle.tsx
@@ -1,11 +1,12 @@
 import { PostsPageQueryType } from "Types/page";
 import { useRouter } from "next/router";
 import React from "react";
+import styles from "./styles.module.scss";
 
 const PostsListTitle = () => {
   const { query } = useRouter();
   const title = getTitle(query);
-  return <h2>{title}</h2>;
+  return <h2 className={styles.PostListTitle}>{title}</h2>;
 };
 
 export default PostsListTitle;
