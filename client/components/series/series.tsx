1589d6cd576a01c4b3943e8d2fa5038142cbb826
BY-juun <neostgeart@gmail.com>
2023-08-24 16:59:19 +0900

    feat: API 호출 위치 수정

--- client/components/series/series.tsx
+++ client/components/series/series.tsx
@@ -1,22 +1,17 @@
-import { useGetAllSeires } from "Hooks/Series";
-import LoadingOrNot from "components/shared/LoadingOrNot";
 import React from "react";
 import SeriesList from "./SeriesList";
-import { AllSeriesAPIType } from "Types/series";
+
 import PageTitle from "components/shared/PageTitle/PageTitle";
 
 const SeriesPageContainer = () => {
-  const { data, isLoading } = useGetAllSeires();
   return (
-    <LoadingOrNot isLoading={isLoading}>
-      <>
-        <PageTitle
-          title="✍️ Series"
-          description="시리즈로 작성된 포스트들을 모아놓았습니다 😄"
-        />
-        <SeriesList data={data as AllSeriesAPIType} />
-      </>
-    </LoadingOrNot>
+    <>
+      <PageTitle
+        title="✍️ Series"
+        description="시리즈로 작성된 포스트들을 모아놓았습니다 😄"
+      />
+      <SeriesList />
+    </>
   );
 };
 
