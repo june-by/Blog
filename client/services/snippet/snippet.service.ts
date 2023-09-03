1bdcc60baee3c06e1ebed69756358f49b5e7e84c
By_juun <neostgeart@gmail.com>
2023-09-03 21:47:13 +0900

    feat: getAllSnippetsAPI, getSnippetAPI

--- client/services/snippet/snippet.service.ts
+++ client/services/snippet/snippet.service.ts
@@ -1,4 +1,4 @@
-import { SnippetFormType } from "Types/snippets";
+import { SnippetFormType, SnippetType } from "Types/snippets";
 import request from "services/request";
 
 export const addSnippetAPI = async (reqData: SnippetFormType) =>
@@ -6,3 +6,9 @@ export const addSnippetAPI = async (reqData: SnippetFormType) =>
 
 export const editSnippetAPI = async ({ snippetId, ...reqData }: SnippetFormType & { snippetId: number }) =>
   request({ method: "patch", url: `/snippet/${snippetId}`, body: reqData });
+
+export const getAllSnippetsAPI = async () =>
+  request<Pick<SnippetType, "id">[]>({ method: "get", url: "/snippet/load/id" });
+
+export const getSnippetAPI = async (snippetId: number) =>
+  request<SnippetType>({ method: "get", url: `/load/${snippetId}` });
