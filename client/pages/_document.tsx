50a5257a78dfb00e818068834e993c455328b685
BY-juun <neostgeart@gmail.com>
2023-09-10 20:03:13 +0900

    feat: font loading in document.tsx

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -22,12 +22,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@800&family=Roboto+Slab:wght@500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&family=Nanum+Myeongjo:wght@800&family=Roboto+Slab:wght@500&display=swap"
             rel="stylesheet"
-          /> */}
+          />
         </Head>
         <body data-theme={theme}>
           <div id="toast-root" />
