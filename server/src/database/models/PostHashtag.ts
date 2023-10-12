c9b5258224a95398a120ccd669b29c23923bed10
BY-juun <neostgeart@gmail.com>
2023-10-12 20:04:57 +0900

    hotfix: PostHashtag 테이블 id 컬럼 제거

--- server/src/database/models/PostHashtag.ts
+++ server/src/database/models/PostHashtag.ts
@@ -1,23 +1,10 @@
-import { DataTypes } from "sequelize";
 import { sequelizeConnection } from "../SequelizeConnection";
 
-export const PostHashtag = sequelizeConnection.define("PostHashtag", {
-  id: {
-    type: DataTypes.INTEGER.UNSIGNED,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-});
+export const PostHashtag = sequelizeConnection.define("PostHashtag", {});
 
 export const initPostHashTag = () => {
   PostHashtag.init(
-    {
-      id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-    },
+    {},
     {
       sequelize: sequelizeConnection,
       tableName: "PostHashtag",
