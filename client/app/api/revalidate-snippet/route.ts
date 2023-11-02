56e3c018bf076f0625854ad4f6e2051012facda7
BY-juun <neostgeart@gmail.com>
2023-11-02 15:45:53 +0900

    feat: api for revalidate app router 전환

---
+++ client/app/api/revalidate-snippet/route.ts
@@ -0,0 +1,27 @@
+import { type NextRequest, type NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const searchParams = req.nextUrl.searchParams;
+  const secret = searchParams.get("secret");
+
+  const body = await req.json();
+
+  if (secret !== process.env.SECRET_REVALIDATE_TOKEN) {
+    return Response.json({ message: "Invalid token" }, { status: 401 });
+  }
+  try {
+    if (!body) {
+      return new Response("Bad reqeust (no body)", { status: 400 });
+    }
+
+    const idToRevalidate = body?.id;
+
+    if (idToRevalidate) {
+      revalidatePath(`/snippets/${idToRevalidate}`);
+      return Response.json({ revalidated: true });
+    }
+  } catch (err) {
+    return new Response("Error while revalidating", { status: 500 });
+  }
+}
