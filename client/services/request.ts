a3f663c942526a8aeed937ca889f3694aeeabdae
BY-juun <neostgeart@gmail.com>
2023-11-03 17:38:28 +0900

    feat: request 내부 구현 axios -> fetch 수정

--- client/services/request.ts
+++ client/services/request.ts
@@ -1,25 +1,49 @@
-import { MESSAGE } from "@constants";
-import { customAxios } from "@utils";
+import { MESSAGE, ServerURL } from "@constants";
 
 interface RequestParams {
   method: "get" | "post" | "patch" | "delete";
   url: string;
   body?: any;
-  onError?: (err: any) => any;
+  cache?: "force-cache" | "no-store";
 }
 
 const request = async <T>({
   method,
   url,
   body,
-  onError,
+  cache,
 }: RequestParams): Promise<T> => {
   try {
-    const { data } = await customAxios[method](url, body);
-    return data;
+    const res = await fetch(`${ServerURL}${url}`, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+      cache,
+      credentials: "include",
+    });
+
+    const isJSONResponse =
+      res.headers.get("content-type")?.indexOf("application/json") !== -1;
+
+    if (isJSONResponse) {
+      const data = await res.json();
+      if (res.ok) {
+        return data;
+      } else {
+        throw new Error(data);
+      }
+    } else {
+      const text = await res.text();
+      if (res.ok) {
+        return text as T;
+      } else {
+        throw new Error(text);
+      }
+    }
   } catch (err: any) {
-    const errorMessage = onError ? onError(err) : err?.response?.data;
-    throw new Error(errorMessage || MESSAGE.NETWORK_ERROR);
+    throw new Error(err?.message || MESSAGE.NETWORK_ERROR);
   }
 };
 
