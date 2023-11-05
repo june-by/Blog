54494b8e2dfe1025685f560adfeab7a536af09d9
BY-juun <neostgeart@gmail.com>
2023-11-05 22:11:41 +0900

    feat: next14버전으로 변경

--- client/next.config.js
+++ client/next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     domains: ["s3.ap-northeast-2.amazonaws.com", "byjuun.com", "*"],
   },
   reactStrictMode: false,
-  experimental: {
-    serverActions: true,
-  },
 };
 
 module.exports = nextConfig;
