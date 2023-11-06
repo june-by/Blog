1aaf3092372e54e1b985a469a615ef4f23b5d5c5
BY-juun <neostgeart@gmail.com>
2023-11-06 16:24:45 +0900

    build server

--- server/build/routes/Snippet/snippetRouter.js
+++ server/build/routes/Snippet/snippetRouter.js
@@ -11,6 +11,6 @@ router.get("/load/id", snippetController_1.default.getAllSnippetsId);
 router.get("/load/all", (0, _middleware_1.cacheMiddleware)(), snippetController_1.default.getAllSnippets);
 router.post("/", _middleware_1.isAdmin, (0, _middleware_1.cacheClearMiddleWare)("snippet"), snippetController_1.default.addSnippet);
 router.get("/load/:snippetId", snippetController_1.default.getSnippet);
-router.patch("/:snippetId", _middleware_1.isAdmin, (0, _middleware_1.cacheClearMiddleWare)("snippet"), snippetController_1.default.updateSnippet);
-router.delete("/:snippetId", _middleware_1.isAdmin, (0, _middleware_1.cacheClearMiddleWare)("snippet"), snippetController_1.default.deleteSnippet);
+router.post("/edit/:snippetId", _middleware_1.isAdmin, (0, _middleware_1.cacheClearMiddleWare)("snippet"), snippetController_1.default.updateSnippet);
+router.post("/delete/:snippetId", _middleware_1.isAdmin, (0, _middleware_1.cacheClearMiddleWare)("snippet"), snippetController_1.default.deleteSnippet);
 exports.default = router;
