ff9fa5d6a2ed1179308c0fa69c48331c8af5d9b4
BY-juun <neostgeart@gmail.com>
2023-11-08 15:50:21 +0900

    feat: PostContent Highlight js Script 제거

--- client/components/post/PostContent/PostContent.tsx
+++ client/components/post/PostContent/PostContent.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Script from "next/script";
 import styles from "./styles.module.scss";
 import "highlight.js/styles/panda-syntax-dark.css";
 import classnames from "classnames";
@@ -8,7 +7,6 @@ import { PostType } from "@Types/post";
 const PostContent = ({ content }: Pick<PostType, "content">) => {
   return (
     <>
-      <Script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js" />
       <article
         className={classnames("Code", styles.PostContent)}
         dangerouslySetInnerHTML={{ __html: content }}
