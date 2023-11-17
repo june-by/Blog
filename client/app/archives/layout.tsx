0fba9aa73979d665123cd7cd32abd588b227c1dc
BY-juun <neostgeart@gmail.com>
2023-11-17 16:53:35 +0900

    feat: 페이지 loading 상태에 spinner 추가

--- client/app/archives/layout.tsx
+++ client/app/archives/layout.tsx
@@ -1,8 +1,17 @@
 import PageLayout from "@components/shared/PageLayout";
+import PageTitle from "@components/shared/PageTitle";
 import React, { PropsWithChildren } from "react";
 
 const ArchiveLayout = ({ children }: PropsWithChildren) => {
-  return <PageLayout type="NonFlex">{children}</PageLayout>;
+  return (
+    <PageLayout type="NonFlex">
+      <PageTitle
+        title="📑 Archives"
+        description="모든 기록들을 한곳에 모아놓은 페이지입니다."
+      />
+      {children}
+    </PageLayout>
+  );
 };
 
 export default ArchiveLayout;
---
+++ client/app/archives/loading.tsx
@@ -0,0 +1,8 @@
+import LoadingSpinner from "@components/shared/LoadingSpinner";
+import React from "react";
+
+const ArchivePageLoading = () => {
+  return <LoadingSpinner />;
+};
+
+export default ArchivePageLoading;
