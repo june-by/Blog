fe2a928db35bd254e403234e0916b917efffed5e
BY-juun <neostgeart@gmail.com>
2023-11-27 00:06:42 +0900

    feat: 무중단배포를 위한 pm2 설정 추가

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
       script: "build/index.js",
       autorestart: false,
       watch: false,
+      wait_ready: true,
+      listen_timeout: 50000,
+      kill_timeout: 5000,
       env: {
         NODE_ENV: "production",
       },
