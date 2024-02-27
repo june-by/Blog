ded2880d05a13737f1407f90bdd1c843e83f2ae6
By_juun <neostgeart@gmail.com>
2024-02-27 22:53:34 +0900

    feat: Snippet 타입에 카테고리 추가

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -108,6 +108,10 @@ export const Snippets = defineDocumentType(() => ({
     lastmod: {
       type: "string",
     },
+    category: {
+      type: "string",
+      required: true,
+    },
   },
   computedFields,
 }));
