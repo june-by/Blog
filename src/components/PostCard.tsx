c8c8a576f1cf8d0ad8766e34cd8dade9264ecc5a
By_juun <neostgeart@gmail.com>
2024-02-28 17:31:30 +0900

    feat: PostCard 반응형 클래스 수정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -7,25 +7,12 @@ import TagList from "./TagList";
 
 const PostCard = (post: Post) => {
   return (
-    <Link
-      key={post._id}
-      href={post.slug}
-      className="block p-0 md:flex w-full gap-6 group"
-    >
-      <figure className="sm: min-h-[11.25rem] rounded-sm overflow-hidden w-full after:content-[''] after:block after:pb-[60%] md:w-[40%] relative">
-        <Image
-          src={post.thumbNail}
-          fill
-          loading="lazy"
-          alt={post.title}
-          placeholder="blur"
-          blurDataURL={blurDataURL}
-        />
+    <Link key={post._id} href={post.slug} className="block p-0 sm:flex w-full gap-6 group">
+      <figure className="sm:min-h-[11.25rem] sm:w-[40%] rounded-sm overflow-hidden w-full after:content-[''] after:block after:pb-[60%] relative">
+        <Image src={post.thumbNail} fill loading="lazy" alt={post.title} placeholder="blur" blurDataURL={blurDataURL} />
       </figure>
       <article className="flex flex-col justify-between w-full p-1 sm:w-[calc(55%-1.5rem)] sm:p-0">
-        <h2 className="group-hover:underline decoration-2 font-bold text-xl sm:text-2xl">
-          {post.title}
-        </h2>
+        <h2 className="group-hover:underline decoration-2 font-bold text-xl sm:text-2xl">{post.title}</h2>
         {post.description && <p className="text-sm">{post.description}</p>}
         <TagList tags={post.tags} />
         <time className="mt-1 text-sm">{formatDate(post.date)}</time>
