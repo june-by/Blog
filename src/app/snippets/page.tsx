8f0e76e078a5d97a3ad49b1c118f9c7797af95a4
By_juun <neostgeart@gmail.com>
2024-03-17 20:47:52 +0900

    feat: use metadata instead of generateMetadata

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -1,22 +1,19 @@
-import ScrollToTopButton from "@/components/shared/ScrollToTopButton";
 import { groupBy } from "@/utils";
 import { allSnippets } from "contentlayer/generated";
 import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
+export const metaData: Metadata = {
+  title: "Snippets | Byjuun.com",
+  description: "개발하면서 사용했던 코드 조각들을 모아놓은 페이지입니다.",
+  openGraph: {
     title: "Snippets | Byjuun.com",
     description: "개발하면서 사용했던 코드 조각들을 모아놓은 페이지입니다.",
-    openGraph: {
-      title: "Snippets | Byjuun.com",
-      description: "개발하면서 사용했던 코드 조각들을 모아놓은 페이지입니다.",
-      url: `https://byjuun.com/snippets`,
-      images: ["/profile.png"],
-    },
-  };
-}
+    url: `https://byjuun.com/snippets`,
+    images: ["/profile.png"],
+  },
+};
 
 const SnippetListPage = () => {
   const snippetsListGroupByCategory = groupBy(allSnippets, "category");
