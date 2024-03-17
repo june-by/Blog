23c3444604a1fea5d97aadff31c0005faf230141
By_juun <neostgeart@gmail.com>
2024-03-17 16:15:18 +0900

    feat: InMemoryStorage 추상화 Class 작성

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -8,6 +8,26 @@ export interface Storage {
   clear(): void;
 }
 
+class MemoStorage implements Storage {
+  private storage = new Map<string, string>();
+
+  public get(key: string) {
+    return this.storage.get(key) || null;
+  }
+
+  public set(key: string, value: string) {
+    this.storage.set(key, value);
+  }
+
+  public remove(key: string) {
+    this.storage.delete(key);
+  }
+
+  public clear() {
+    this.storage.clear();
+  }
+}
+
 class LocalStorage implements Storage {
   public static canUse(): boolean {
     const TEST_KEY = generateTestKey();
