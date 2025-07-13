3e121f4efc0b67060102892ebf6a8bdb217daa51
june-by <neostgeart@gmail.com>
2025-07-13 22:22:16 +0900

    feat: 헤더 아이콘 위치, 링크 수정

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ModeToggle } from "../ModeToggle";
+import Link from "next/link";
 import RenderAfterMounted from "./RenderAfterMounted";
+import { FaGithub, FaHome, FaLinkedin } from "react-icons/fa";
 
 const header = () => {
   return (
@@ -9,6 +11,17 @@ const header = () => {
         <RenderAfterMounted fallback={<div />}>
           <ModeToggle />
         </RenderAfterMounted>
+        <div className="flex gap-2 items-center">
+          <Link href="/" className="ml-auto text-lg font-medium space-x-6" >
+            <FaHome />
+          </Link>
+          <Link href="https://github.com/june-by" className="ml-auto text-lg font-medium space-x-6" target="_blank">
+            <FaGithub />
+          </Link>
+          <Link href="https://www.linkedin.com/in/byongjun-ahn-20168b252/" className="ml-auto text-lg font-medium space-x-6" target="_blank">
+            <FaLinkedin />
+          </Link>
+        </div>
       </div>
     </header>
   );
